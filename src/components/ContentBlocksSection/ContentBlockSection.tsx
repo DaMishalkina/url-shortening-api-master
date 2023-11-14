c380f6773a740b9384dc5a0664f5c8f663353cdd
Daria Mishalkina <dashaio251@gmail.com>
2023-11-14 13:53:42 +0100

    content block section mobile layout

---
+++ src/components/ContentBlocksSection/ContentBlockSection.tsx
@@ -0,0 +1,66 @@
+import "./ContentBlockSection.scss";
+
+interface Props {
+    title?: string,
+    text?: string,
+    blocks?: Block[]
+}
+
+type Block = {
+    title?: string,
+    text?: string,
+    image?: {
+        alt: string,
+        src: string
+    }
+}
+
+export const ContentBlockSection = ({
+                                        title,
+                                        text,
+                                        blocks
+                                    }: Props) => {
+    return (
+        <section className="content-block-section">
+            {(title || text) && (
+                <div className="content-block-section__text-container">
+                    {title && (
+                        <h2 className="content-block-section__title">{title}</h2>
+                    )}
+                    {text && (
+                        <p className="content-block-section__text">{text}</p>
+                    )}
+                </div>
+            )}
+            {blocks && (
+               <ul className="content-block-section__list">
+                   {blocks.map(({title, text, image}, index) => (
+                       <li className="content-block-item content-block-section__item" key={title || index}>
+                           {index !== 0 && (
+                               <div className="content-block-item__line"></div>
+                           )}
+                           <div className="content-block-item__container">
+                               {image && (
+                                   <div className="content-block-item__image-container">
+                                       <img
+                                           className="content-block-item__image"
+                                           alt={image.alt}
+                                           src={image.src}
+                                       />
+                                   </div>
+                               )}
+                               {title && (
+                                   <p className="content-block-item__title">{title}</p>
+                               )}
+                               {text && (
+                                   <p className="content-block-item__text">{text}</p>
+                               )}
+                           </div>
+                       </li>
+                   ))}
+               </ul>
+
+            )}
+        </section>
+    )
+}
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -4,12 +4,17 @@ import {Button} from "../../components/Button/Button";
 import {CustomLink} from "../../components/CustomLink/CustomLink";
 
 import pageContent from "../../data/pageContent.json";
+import {ContentBlockSection} from "../../components/ContentBlocksSection/ContentBlockSection";
 import {HeroSection} from "../../components/HeroSection/HeroSection";
+
 
 
 export const Home = () => {
     const [copyText, setCopyText] = useState("");
     const [isCopied, setIsCopied] = useState(false);
+    const statisticsCards = pageContent?.statisticsSection?.cards.map(card => {
+        return {...card, image: {alt: card.image, src: `src/assets/${card.image}.svg`}}
+    })
     async function copyTextToClipboard(text: string) {
         if ('clipboard' in navigator) {
             return await navigator.clipboard.writeText(text);
@@ -40,6 +45,11 @@ export const Home = () => {
                 text={pageContent?.heroSection?.text}
                 image={pageContent?.heroSection?.image}
                 link={pageContent?.heroSection?.link}
+            />
+            <ContentBlockSection
+                title={pageContent?.statisticsSection?.title}
+                text={pageContent?.statisticsSection?.text}
+                blocks={statisticsCards}
             />
         </section>
     )
